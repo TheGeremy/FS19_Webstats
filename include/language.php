4aba92cffb0f74ea20cb38809cc24d18946ec324
TheGeremy <the.geremy@gmail.com>
2019-09-03 17:21:12 +0200

    Two Commits by J0hnHawk
    Support products of GlobalCompany
    Workaround if HTTP_ACCEPT_LANGUAGE does not exists

--- include/language.php
+++ include/language.php
@@ -22,7 +22,12 @@ if (! defined ( 'IN_FS19WS' )) {
 	exit ();
 }
 
-$languageFromBrowser = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+if (isset ( $_SERVER ['HTTP_ACCEPT_LANGUAGE'] )) {
+	$languageFromBrowser = substr ( $_SERVER ['HTTP_ACCEPT_LANGUAGE'], 0, 2 );
+} else {
+	$languageFromBrowser = 'en';
+}
+
 if (file_exists ( './language/' . $languageFromBrowser . '/global.lng' )) {
 	$defaultLanguage = $languageFromBrowser;
 } else {
